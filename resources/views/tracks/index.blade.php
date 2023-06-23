ee5a0a22e308cc0cd59f32a9b1962dc956ec7681
RosaCArmen <rosamarcapaco@gmail.com>
2023-06-22 23:08:26 -0400

    app musica

---
+++ resources/views/tracks/index.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between" >
+            <h2>
+                Listado de canciones
+            </h2>
+            <a href="{{ route('tracks.create') }}" class="bg-blue-500 rounded px-4 py-2">
+                Nueva Cancion
+            </a>
+        </div>
+    </x-slot>
+    <div>
+        <div class="grid grid-cols-8 gap-2">
+            @foreach ($tracks as $track)
+            <div class="rounded bg-blue-200 p-4">
+                {{ $track->title }}
+            </div>
+        @endforeach
+        </div>
+    </div>
+</x-app-layout>
